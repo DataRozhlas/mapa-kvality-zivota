22497c6a4f87437067fe37ac0889e0d4a2f88835
datastory.cz <honza@datastory.cz>
2018-11-16 15:15:53 +0100

    alfa

---
+++ article.md
@@ -0,0 +1,46 @@
+title: "Data z mobilů:<br>Praha se denně nafoukne o polovinu, v centru jsou návštěvníci v převaze"
+perex: "Skoro 140 tisíc lidí přijede do Prahy každý den za prací. Míří hlavně do kancelářských budov a do centra města. S nimi pak dorazí i přes půl milionu návštěvníků. Ukázala to analýza dat o pohybu mobilních telefonů, kterou si nyní město nechává zpracovat."
+published: "21. dubna 2018"
+coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
+coverimg_note: "Foto <a href='#'>ČTK</a>"
+styles: [https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.2.0/css/ol.css]
+libraries: [jquery, https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.2.0/build/ol.js, https://openmaptiles.org/maps/olms.js, https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js, ] #jquery, d3, highcharts, datatables
+options: [noheader, nopic] #wide, noheader (, nopic)
+---
+<left>
+	<p>
+	<b>KAREL HYNEK MÁCHA</b>
+	</p><p>
+	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
+	</p>
+</left>
+
+Během dne se lidé v Praze soustřeďují v okolí administrativních center a obchodů, po poledni se tak nejvíc zahustí okolí Andělu, centrum Prahy (Vodičkova ulice a Petrské náměstí) a Brumlovka nedaleko Budějovické. Na poslední jmenované adrese sídlí řada velkých korporací, mezi nimi i Microsoft nebo ČEZ. Pro srovnání, na zmíněných místech je ve „špičce“ okolo šesti stovek osob na jeden hektar, průměr Prahy je 25 obyvatel na [hektar](https://cs.wikipedia.org/wiki/Hektar) (Václavské náměstí má rozlohu asi 4 hektary).
+
+Naopak v noci se nejvíc lidí se „namačká“ ve Vršovicích, jde o 430 osob na hektar.
+
+Detaily si můžete prohlédnout v následující mapě, kterou z dat mobilních operátorů zpracoval pražský [Institut plánování a rozvoje](http://www.iprpraha.cz/).
+
+<wide>
+<div id="cont">
+    <div id="sliders"></div>
+    <div id="map"></div>
+</div>
+<div id="tooltip">Vyberte obec v mapě.</div>
+</wide>
+
+
+## Data na prodej
+<right>
+	<p>
+	<b>KAREL HYNEK MÁCHA</b>
+	</p><p>
+	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
+	</p>
+</right>
+
+I pokud netelefonujeme nebo neposíláme SMS, telefon pravidelně komunikuje s vysílači v síti mobilního operátora. Z dat o síle signálu a použité anténě je možné odvodit, kde se přibližně telefon nachází a jak dlouhou dobu na místě strávil.
+
+Mobilní operátor má [zákonnou povinnost](https://www.zakonyprolidi.cz/cs/2005-127/zneni-20160919#p97-3) takové informace uchovávat, a to půl roku do minulosti, přistupovat k nim ale mohou jen bezpečnostní složky, typicky policie nebo rozvědka.
+
+Operátoři ale obdobné informace mohou anonymizovat (tedy zbavit identifikace konkrétního uživatele) a agregovat, tedy spojit informace o jednotlivcích do jakýchsi skupin. Výsledná obecná čísla pak přeprodávají dál, například marketingovým firmám či dopravním analytikům. Zpětně z nich nejde dovodit, kde se pohybuje každý jednotlivec, dávají ale určitý přehled o obecném chování obyvatel nějakého místa.
---
+++ js/z_mapa.js
@@ -0,0 +1,191 @@
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+var isEdge = !isIE && !!window.StyleMedia;
+
+var base = new ol.layer.VectorTile({
+    declutter: true,
+    source: new ol.source.VectorTile({
+        attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
+        '© <a href="https://www.openstreetmap.org/copyright">' +
+        'OpenStreetMap contributors</a>',
+        format: new ol.format.MVT(),
+        url: 'https://data.irozhlas.cz/tiles/{z}/{x}/{y}.pbf'
+    })
+});
+
+var tema = new ol.layer.VectorTile({
+    declutter: true,
+    source: new ol.source.VectorTile({
+        format: new ol.format.MVT(),
+        url: './tiles/{z}/{x}/{y}.pbf'
+    })
+});
+
+var map = new ol.Map({
+    target: 'map',
+    interactions: ol.interaction.defaults({mouseWheelZoom:false}),
+    view: new ol.View({
+        center: ol.proj.transform([15.3350758, 49.7417517], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 7
+    })
+});
+
+olms.applyStyle(base, positronStyle, 'openmaptiles').then(function() {
+    map.addLayer(base);
+    map.addLayer(tema);
+});
+
+var cols = {
+    'nezam': 'Nezaměstnanost',
+    'emise': 'Emise',
+    'doziti_m': 'Naděje dožití mužů',
+    'rozvody': 'Index rozvodovosti',
+    'ned_ms': 'Nedostuopnost MŠ',
+    'ned_ss': 'Nedostupnost SŠ',
+    'ned_zdrav': 'Nedostupnost zdrav. zař.',
+    'exe': 'Exekucí na obyv.',
+    'prumysl': 'Podíl prac. v průmyslu',
+    'bezpecnost': 'Bezpečnost',
+    'prirust': 'Přírůstek obyvatel',
+    'verici': 'Procento věřících',
+    'ned_net': 'Nedostupnost internetu',
+    'vz_okr': 'Vzdálenost k okresnímu městu'
+};
+
+var radios = '<h3>Jak důležité jsou pro vás následující parametry?</h3>';
+Object.keys(cols).forEach(function(key){
+    radios += '<p>' + cols[key] + '<form action="" method="post">'
+        + '<input type="radio" id="0_' + key + '" name="' + key + '" value="0" />'
+        + '<label for="0_' + key + '">Vůbec</label>'
+        + '<input type="radio" id="05_' + key + '" name="' + key + '" value="1" checked />'
+        + '<label for="05_' + key + '">Trochu</label>'
+        + '<input type="radio" id="1_' + key + '" name="' + key + '" value="1.5"/>'
+        + '<label for="1_' + key + '">Hodně</label>'
+        + '</form></p>'
+});
+
+document.getElementById('sliders').innerHTML = radios;
+
+$('input[type=radio]').change(function(e) {
+    koef_user[e.currentTarget.name] = parseFloat(e.currentTarget.defaultValue);
+    tema.changed()
+});
+
+var koef = {
+    'exe': 0.02407,
+    'nezam': 0.01684,
+    'emise': 0.01175,
+    'ned_ms': 0.00895,
+    'vz_okr': 0.00866,
+    'ned_zdrav': 0.00843,
+    'ned_ss': 0.00797,
+    'prumysl': 0.00768,
+    'ned_net': 0.00651,
+    'rozvody': 0.00459,
+    'verici': -0.0049,
+    'bezpecnost': -0.01194,
+    'prirust': -0.01473,
+    'doziti_m': -0.01692
+};
+
+ var koef_user = {
+    'nezam': 1,
+    'emise': 1,
+    'doziti_m': 1,
+    'rozvody': 1,
+    'ned_ms': 1,
+    'ned_ss': 1,
+    'ned_zdrav': 1,
+    'exe': 1,
+    'prumysl': 1,
+    'bezpecnost': 1,
+    'prirust': 1,
+    'verici': 1,
+    'ned_net': 1,
+    'vz_okr': 1
+ };
+
+var scl = d3.scale.linear().domain([0.15695, 0.66587]).range([0, 1]);
+var colScl = d3.scale.threshold().domain([0.2, 0.4, 0.6, 0.8]).range([
+    'rgba(254,229,217,0.8)',
+    'rgba(252,174,145,0.8)',
+    'rgba(251,106,74,0.8)',
+    'rgba(222,45,38,0.8)',
+    'rgba(165,15,21,0.8)'
+]);
+
+function getIndex(ftr) {
+    var ob = data[ftr.Kod];
+    if (ob == undefined) {
+        return [-1, null]
+    };
+    var index = 0.50519; //intercept
+    Object.keys(cols).forEach(function(c) {
+        index += (ob[c] * koef[c] * koef_user[c]);
+    })
+    return [scl(1 - index), ob];
+};
+
+function getColor(ftr) {
+    var style = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: "lightgray",
+      width: 0.3
+    }),
+    fill: new ol.style.Fill({
+      color: colScl(getIndex(ftr)[0]),
+      opacity: 0.5
+    })
+  })
+  return style;
+};
+
+var tema = new ol.layer.VectorTile({
+    declutter: true,
+    source: new ol.source.VectorTile({
+        attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
+        '© <a href="https://www.openstreetmap.org/copyright">' +
+        'OpenStreetMap contributors</a>',
+        format: new ol.format.MVT(),
+        url: './tiles/{z}/{x}/{y}.pbf'
+    }),
+    style: function(feature) {
+      return getColor(feature.properties_)
+    }
+});
+
+function makeTooltip(ftr) {
+    var out = getIndex(ftr)
+    document.getElementById('tooltip').innerHTML = out[1].nazev + ' (okres ' + out[1].okres + ')<br>ASPEN index: '+ Math.round(out[0] * 1000) / 10;
+}
+
+if (!(isEdge | isFirefox | isIE)) {
+    map.on('pointermove', function(evt) {
+      if (evt.dragging) {
+        return;
+      }
+      var pixel = map.getEventPixel(evt.originalEvent);
+        if (map.hasFeatureAtPixel(pixel)){
+            map.forEachFeatureAtPixel(pixel, function(feature) {
+                if (Object.keys(feature.properties_).indexOf('Kod') != -1) {
+                    makeTooltip(feature.properties_);
+                }
+            });
+        } else {
+            document.getElementById('tooltip').innerHTML = 'Vyberte obec'
+        }
+        });
+  };
+
+map.on('singleclick', function(evt) {
+    var pixel = map.getEventPixel(evt.originalEvent);
+    if (map.hasFeatureAtPixel(pixel)){
+        map.forEachFeatureAtPixel(pixel, function(feature) {
+            if (Object.keys(feature.properties_).indexOf('Kod') != -1) {
+                makeTooltip(feature.properties_);
+            }
+        });
+    } else {
+        document.getElementById('tooltip').innerHTML = 'Vyberte obec'
+    }
+});
